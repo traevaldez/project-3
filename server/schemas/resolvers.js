a301381b058e933109679d60283aeb337d76f773
Maximoo <maximtz99@gmail.com>
2022-12-14 20:01:32 -0600

    minor tweaks

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Thought } = require('../models/asdwd');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
